9998e79731a7fba59d9002d5f914a6e76ae8befd
KudryavtsevIvan <113138605+KudryavtsevIvan@users.noreply.github.com>
2022-12-21 23:30:33 +0300

    Update repository.go

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,9 +1,9 @@
 package repository
 
-import app "github.com/KudryavtsevIvan/city_information_service"
+import cities "github.com/KudryavtsevIvan/city_information_service"
 
 type CityList interface {
-	Create(city app.CityRequest) (string, error)
+	Create(city cities.CityRequest) (string, error)
 	Delete(id int) error
 	SetPopulation(id, population int) error
 	GetFromRegion(region string) ([]string, error)
@@ -11,7 +11,7 @@ type CityList interface {
 	GetFromPopulation(start, end int) ([]string, error)
 	GetFromFoundation(start, end int) ([]string, error)
 	findCities(idx_type int, searchText string) []string
-	GetFull(id int) (*app.City, error)
+	GetFull(id int) (*cities.City, error)
 }
 
 type Repository struct {
