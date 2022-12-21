32abf078e32f7b71cf4cdb891f9777fe98ea530e
KudryavtsevIvan <113138605+KudryavtsevIvan@users.noreply.github.com>
2022-12-21 22:43:53 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -33,7 +33,7 @@ func main() {
 	handlers := handler.NewHandler(services)
 
 	log.Info().Msg("Starting server...")
-	server := new(app.Server)
+	server := new(cities.Server)
 	go func() {
 		if err := server.Run(viper.GetString("server.port"), handlers.InitRoutes()); err != nil {
 			log.Err(err).Msg("Server is not running")
