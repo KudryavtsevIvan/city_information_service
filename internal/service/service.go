6470e85276954127e4811c3e7251ce20afbd3300
KudryavtsevIvan <113138605+KudryavtsevIvan@users.noreply.github.com>
2022-12-22 22:47:35 +0300

    Update service.go

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,19 +1,19 @@
 package service
 
 import (
-	cities "github.com/KudryavtsevIvan/city_information_service/internal/app"
+	model "github.com/KudryavtsevIvan/city_information_service/internal/model"
 	"github.com/KudryavtsevIvan/city_information_service/internal/repository"
 )
 
 type City interface {
-	Create(city cities.CityRequest) (string, error)
+	Create(city model.CityRequest) (string, error)
 	Delete(id int) error
 	SetPopulation(id, population int) error
 	GetFromRegion(region string) ([]string, error)
 	GetFromDistrict(district string) ([]string, error)
 	GetFromPopulation(start, end int) ([]string, error)
 	GetFromFoundation(start, end int) ([]string, error)
-	GetFull(id int) (*cities.City, error)
+	GetFull(id int) (*model.City, error)
 }
 
 type Service struct {
